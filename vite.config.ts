804944a45e31b06193bf7d4fb0da654e49a94fed
Ceiridge <philipp34533@icloud.com>
2023-12-16 22:31:18 +0100

    Convert SvelteKit to Svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import {svelte} from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [svelte()],
+	base: "./"
 });
